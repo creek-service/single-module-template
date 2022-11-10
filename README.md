77cdc746f86a9f908944fdcae4ed32b6724632df
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2022-11-10 00:26:46 +0000

    Switch to new build conventions (#56)
    * Switch to new build conventions
    
    * MErge

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <!-- ChangeMe: replace /single-module-template in the badge urls below with the name of the repo-->
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 [![Coverage Status](https://coveralls.io/repos/github/creek-service/single-module-template/badge.svg?branch=main)](https://coveralls.io/github/creek-service/single-module-template?branch=main)
-[![build](https://github.com/creek-service/single-module-template/actions/workflows/gradle.yml/badge.svg)](https://github.com/creek-service/single-module-template/actions/workflows/gradle.yml)
+[![build](https://github.com/creek-service/single-module-template/actions/workflows/build.yml/badge.svg)](https://github.com/creek-service/single-module-template/actions/workflows/build.yml)
 [![CodeQL](https://github.com/creek-service/single-module-template/actions/workflows/codeql.yml/badge.svg)](https://github.com/creek-service/single-module-template/actions/workflows/codeql.yml)
 
 # single-module template Repo
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,75 +1,28 @@
 plugins {
     java
     jacoco
-    `maven-publish`
-    id("com.github.spotbugs") version "5.0.13"                   // https://plugins.gradle.org/plugin/com.github.spotbugs
-    id("com.diffplug.spotless") version "6.11.0"                 // https://plugins.gradle.org/plugin/com.diffplug.spotless
-    id("pl.allegro.tech.build.axion-release") version "1.14.2"  // https://plugins.gradle.org/plugin/pl.allegro.tech.build.axion-release
-    id("com.github.kt3k.coveralls") version "2.12.0"            // https://plugins.gradle.org/plugin/com.github.kt3k.coveralls
-    id("org.javamodularity.moduleplugin") version "1.8.12"      // https://plugins.gradle.org/plugin/org.javamodularity.moduleplugin
+    `creek-common-convention`
+    `creek-module-convention`
+    `creek-coverage-convention`
+    `creek-publishing-convention`
+    `creek-sonatype-publishing-convention`
+    id("pl.allegro.tech.build.axion-release") version "1.14.2" // https://plugins.gradle.org/plugin/pl.allegro.tech.build.axion-release
 }
 
 project.version = scmVersion.version
 
-apply(plugin = "idea")
-apply(plugin = "java")
-apply(plugin = "jacoco")
-apply(plugin = "checkstyle")
-apply(plugin = "com.diffplug.spotless")
-apply(plugin = "com.github.spotbugs")
-apply(plugin = "maven-publish")
-
-group = "org.creekservice"
-
-java {
-    withSourcesJar()
-
-    modularity.inferModulePath.set(false)
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-repositories {
-    mavenCentral()
-
-    maven {
-        url = uri("https://maven.pkg.github.com/creek-service/*")
-        credentials {
-            username = "Creek-Bot-Token"
-            password = "\u0067hp_LtyvXrQZen3WlKenUhv21Mg6NG38jn0AO2YH"
-        }
-    }
-}
-
-configurations.all {
-    resolutionStrategy.cacheChangingModulesFor(10, TimeUnit.MINUTES)
-}
-
-extra.apply {
-    set("creekBaseVersion", "0.2.0-SNAPSHOT")
-    set("creekTestVersion", "0.2.0-SNAPSHOT")
-    set("spotBugsVersion", "4.6.0")         // https://mvnrepository.com/artifact/com.github.spotbugs/spotbugs-annotations
-
-    set("log4jVersion", "2.19.0")           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
-    set("guavaVersion", "31.1-jre")         // https://mvnrepository.com/artifact/com.google.guava/guava
-    set("junitVersion", "5.9.1")            // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
-    set("junitPioneerVersion", "1.7.1")     // https://mvnrepository.com/artifact/org.junit-pioneer/junit-pioneer
-    set("mockitoVersion", "4.8.1")          // https://mvnrepository.com/artifact/org.mockito/mockito-junit-jupiter
-    set("hamcrestVersion", "2.2")           // https://mvnrepository.com/artifact/org.hamcrest/hamcrest-core
-}
-
-val creekTestVersion : String by extra
-val guavaVersion : String by extra
-val log4jVersion : String by extra
-val junitVersion: String by extra
-val junitPioneerVersion: String by extra
-val mockitoVersion: String by extra
-val hamcrestVersion : String by extra
+val creekVersion = "0.2.0-SNAPSHOT"
+val guavaVersion = "31.1-jre"         // https://mvnrepository.com/artifact/com.google.guava/guava
+val log4jVersion = "2.19.0"           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
+val junitVersion = "5.9.1"            // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
+val junitPioneerVersion = "1.7.1"     // https://mvnrepository.com/artifact/org.junit-pioneer/junit-pioneer
+val mockitoVersion = "4.8.0"          // https://mvnrepository.com/artifact/org.mockito/mockito-junit-jupiter
+val hamcrestVersion = "2.2"           // https://mvnrepository.com/artifact/org.hamcrest/hamcrest-core
 
 dependencies {
-    testImplementation("org.creekservice:creek-test-hamcrest:$creekTestVersion")
-    testImplementation("org.creekservice:creek-test-util:$creekTestVersion")
-    testImplementation("org.creekservice:creek-test-conformity:$creekTestVersion")
+    testImplementation("org.creekservice:creek-test-hamcrest:$creekVersion")
+    testImplementation("org.creekservice:creek-test-util:$creekVersion")
+    testImplementation("org.creekservice:creek-test-conformity:$creekVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testImplementation("org.junit-pioneer:junit-pioneer:$junitPioneerVersion")
@@ -79,101 +32,6 @@ dependencies {
     testImplementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     testImplementation("org.apache.logging.log4j:log4j-slf4j2-impl:$log4jVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-}
-
-tasks.compileJava {
-    options.compilerArgs.add("-Xlint:all,-serial,-requires-automatic,-requires-transitive-automatic")
-    options.compilerArgs.add("-Werror")
-}
-
-tasks.test {
-    useJUnitPlatform()
-    setForkEvery(1)
-    maxParallelForks = 4
-    testLogging {
-        showStandardStreams = true
-        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-        showCauses = true
-        showExceptions = true
-        showStackTraces = true
-    }
-}
-
-spotless {
-    java {
-        googleJavaFormat("1.12.0").aosp()
-        indentWithSpaces()
-        importOrder()
-        removeUnusedImports()
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
-}
-
-spotbugs {
-    tasks.spotbugsMain {
-        reports.create("html") {
-            required.set(true)
-            setStylesheet("fancy-hist.xsl")
-        }
-    }
-    tasks.spotbugsTest {
-        reports.create("html") {
-            required.set(true)
-            setStylesheet("fancy-hist.xsl")
-        }
-    }
-}
-
-tasks.withType<JacocoReport>().configureEach{
-    reports {
-        xml.required.set(true)
-    }
-
-    dependsOn(tasks.test)
-}
-
-tasks.register("format") {
-    dependsOn("spotlessCheck", "spotlessApply")
-}
-
-tasks.register("static") {
-    dependsOn("checkstyleMain", "checkstyleTest", "spotbugsMain", "spotbugsTest")
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/creek-service/${rootProject.name}")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-
-            pom {
-                url.set("https://github.com/creek-service/${rootProject.name}.git")
-            }
-        }
-    }
-}
-
-tasks.register("coverage") {
-    group = "coverage"
-    dependsOn("jacocoTestReport")
-}
-
-tasks.coveralls {
-    group = "coverage"
-    description = "Uploads the aggregated coverage report to Coveralls"
-
-    dependsOn("coverage")
-    onlyIf{System.getenv("CI") != null}
 }
 
 defaultTasks("format", "static", "check")
