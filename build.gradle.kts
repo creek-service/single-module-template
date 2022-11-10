639d0c75c96df7477ee73b847156440b65a6c1e0
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-11-10 00:13:44 +0000

    MErge

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ project.version = scmVersion.version
 
 val creekVersion = "0.2.0-SNAPSHOT"
 val guavaVersion = "31.1-jre"         // https://mvnrepository.com/artifact/com.google.guava/guava
-val log4jVersion = "2.18.0"           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
+val log4jVersion = "2.19.0"           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
 val junitVersion = "5.9.1"            // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
 val junitPioneerVersion = "1.7.1"     // https://mvnrepository.com/artifact/org.junit-pioneer/junit-pioneer
 val mockitoVersion = "4.8.0"          // https://mvnrepository.com/artifact/org.mockito/mockito-junit-jupiter
